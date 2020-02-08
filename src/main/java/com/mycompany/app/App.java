0d0ca00ba4f166d73df506846d42f989e62bd940
uygurugurlu <uygurugurlu@gmail.com>
2020-02-08 18:47:45 +0300

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,15 @@
 package com.mycompany.app;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 /**
  * Hello world!
  *
@@ -8,16 +18,40 @@ public class App
 {
     public static void main( String[] args )
     {
-        int[] p1 = new int[10];
-        int[] p2 = new int[10];
-        String s1= "";
-        String s2= "";
-        for(int i = 0; i<p1.length; i++){
-            p1[i] = (int)(Math.random()*25)+97; 
-            p2[i] = (int)(Math.random()*25)+97; 
+        port(getHerokuAssignedPort());
+        get("/", (req, res) -> "Hello, World");
+        post("/compute", (req, res) -> {
+            //System.out.println(req.queryParams("input1"));
+            //System.out.println(req.queryParams("input2"));
+  
+            String input1 = req.queryParams("input1");
+            String input2 = req.queryParams("input2");
+            String s1 = "selam";
+            String s2 = "merhaba";
+            String[] inp1list = input1.split(",");
+            String[] inp2list = input2.split(",");
+            int[] player1 = new int[inp1list.length];
+            int[] player2 = new int[inp1list.length];
+            for(int i =0; i<player1.length; i++){
+                player1[i] = Integer.parseInt(inp1list[i]);
+            }
+            for(int i =0; i<player2.length; i++){
+                player2[i] = Integer.parseInt(inp2list[i]);
+            }
+            String result = guessGame(player1, player2, s1, s2);
+           Map map = new HashMap();
+            map.put("result", result);
+            return new ModelAndView(map, "compute.mustache");
+          }, new MustacheTemplateEngine());
+  
+          get("/compute",
+          (rq, rs) -> {
+            Map map = new HashMap();
+            map.put("result", "not computed yet!");
+            return new ModelAndView(map, "compute.mustache");
+          },
+          new MustacheTemplateEngine());
 
-        }
-        System.out.println(guessGame(p1,p2,s1,s2));
     }
 
     public static String guessGame(int[] player1, int[] player2, String s1, String s2){
@@ -43,4 +77,12 @@ public class App
         } 
         else return null;
     }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 }
